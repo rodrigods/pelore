cdb2248143d43bb6277391cebcb2c3aeddb0541d
thiagossantos <thiagossantos@75052923-3742-0410-8fdd-33ae7300e995>
2007-12-27 22:24:42 +0000

    ItemLoan finished. Created Util as a helper class.
    Stub tests for ItemLoan, Util and Loan.

---
+++ pelore/test/test/unit/LoanTest.java
@@ -0,0 +1,84 @@
+package test.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoanTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLoanPerson() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLoanPersonString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLoanPersonStringDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLoanPersonDateDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLoanPersonStringDateDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetPerson() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetComment() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetLoanDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetExpectedDevolutionDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCloseLoan() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetDevolutionDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
