96ecb2a604f640af38621bafdd52b687d2556779
thiagossantos <thiagossantos@75052923-3742-0410-8fdd-33ae7300e995>
2007-12-27 21:56:01 +0000

    Loan modifications; ItemLoan creation.

---
+++ pelore/src/pelore/ItemLoan.java
@@ -0,0 +1,44 @@
+package pelore;
+
+import java.util.Date;
+
+public class ItemLoan extends Loan {
+
+	private Item loanedItem;
+	
+	public ItemLoan(Person p, Date loanDate, Date devolutionDate, Item item, String comment) throws ItemLoanException {
+		super(p, comment, loanDate, devolutionDate);
+		setItem(item);
+	}
+
+	public ItemLoan(Person p, Date loanDate, Date devolutionDate, Item item) throws ItemLoanException {
+		this(p, loanDate, devolutionDate, item, null);
+	}
+	
+	public ItemLoan(Person p, Item item) throws ItemLoanException {
+		this(p, null, null, item, null);
+	}
+	
+	private void setItem(Item item) throws ItemLoanException {
+		if (item == null) {
+			throw new IllegalArgumentException("Item cannot be null");
+		}
+		if (item.isLended()) {
+			throw new ItemLoanException("Item is already lended", item);
+		}
+		
+		this.loanedItem = item;
+	}
+	
+	public Item getItem() {
+		return loanedItem;
+	}
+	
+	public void closeLoan() {
+		if (!super.isClosed()) {
+			super.closeLoan();
+			this.loanedItem.setLended(false);
+		}
+	}
+
+}
--- pelore/src/pelore/Loan.java
+++ pelore/src/pelore/Loan.java
@@ -1,36 +1,48 @@
 package pelore;
 import java.util.Date;
 
-//TODO - add closeLoan method (and a variable to the loan date finishing)
-
 public abstract class Loan {
 
+	private Person person;
 	private String comment;
 	private Date loanDate;
+	private Date expectedDevolutionDate;
 	private Date devolutionDate;
 	
-	public Loan() {
-		this(null, null, null);
+	public Loan(Person p) {
+		this(p, null, null, null);
 	}
 	
-	public Loan(String comment) {
-		this(comment, null, null);
+	public Loan(Person p, String comment) {
+		this(p, comment, null, null);
 	}
 	
-	public Loan(String comment, Date devolutionDate) {
-		this(comment, null, devolutionDate);
+	public Loan(Person p, String comment, Date expectedDevolutionDate) {
+		this(p, comment, null, expectedDevolutionDate);
 	}
 	
-	public Loan(Date loanDate, Date devolutionDate) {
-		this(null, loanDate, devolutionDate);
+	public Loan(Person p, Date loanDate, Date expectedDevolutionDate) {
+		this(p, null, loanDate, expectedDevolutionDate);
 	}
 	
-	public Loan(String comment, Date loanDate, Date devolutionDate) {
+	public Loan(Person p, String comment, Date loanDate, Date expectedDevolutionDate) {
+		setPerson(p);
 		this.comment = comment;
 		this.loanDate = loanDate;
-		this.devolutionDate = devolutionDate;
+		this.expectedDevolutionDate = expectedDevolutionDate;
 	}
 
+	public void setPerson(Person p) {
+		if (p == null) {
+			throw new IllegalArgumentException("Person is null");
+		}
+		this.person = p;
+	}
+	
+	public Person getPerson() {
+		return person;
+	}
+	
 	public String getComment() {
 		return comment;
 	}
@@ -47,12 +59,41 @@ public abstract class Loan {
 		this.loanDate = loanDate;
 	}
 
+	public Date getExpectedDevolutionDate() {
+		return expectedDevolutionDate;
+	}
+
+	public void setExpectedDevolutionDate(Date expectedDevolutionDate) {
+		this.expectedDevolutionDate = expectedDevolutionDate;
+	}
+	
+	public void closeLoan() {
+		if (!isClosed()) {
+			this.devolutionDate = new Date();
+		}
+	}
+	
+	public boolean isClosed() {
+		return this.devolutionDate != null;
+	}
+	
 	public Date getDevolutionDate() {
 		return devolutionDate;
 	}
-
-	public void setDevolutionDate(Date devolutionDate) {
-		this.devolutionDate = devolutionDate;
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof Loan) {
+			Loan other = (Loan) obj;
+			return other.getPerson().equals(this.getPerson()) &&
+					Util.equalObjects(other.getLoanDate(), this.getLoanDate()) &&
+					Util.equalObjects(other.getExpectedDevolutionDate(), this.getExpectedDevolutionDate()) &&
+					Util.equalObjects(other.getDevolutionDate(), this.getDevolutionDate()) &&
+					Util.equalObjects(other.getComment(), this.getComment());
+		}
+		return false;
 	}
 	
+	public String toString() {
+		return getPerson().toString() + ", " + "loan date: " + getLoanDate() + " " + (isClosed() ? "(returned item)" : "(not returned yet)");  
+	}
 }
