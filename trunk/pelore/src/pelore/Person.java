b69e59b1a11e58e7690a166383bf538987bd9596
thiagossantos <thiagossantos@75052923-3742-0410-8fdd-33ae7300e995>
2007-12-27 10:38:03 +0000

    Adds person class and stub tests for it.

---
+++ trunk/pelore/src/pelore/Person.java
@@ -0,0 +1,82 @@
+package pelore;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Person {
+
+	private List<PhoneNumber> phoneNumbers;
+	private String name;
+	private Address address;
+	
+	public Person(String name) {
+		setName(name);
+		phoneNumbers = new LinkedList<PhoneNumber>();
+	}
+	
+	public Person(String name, Address ad) {
+		this(name);
+		setAddress(ad);
+	}
+	
+	public void addPhoneNumber(PhoneNumber pn, int pos) {
+		phoneNumbers.add(pos, pn);
+	}
+	
+	public void removePhoneNumber(int pos) {
+		phoneNumbers.remove(pos);
+	}
+	
+	public void removePhoneNumber(PhoneNumber pn) {
+		phoneNumbers.remove(pn);
+	}
+	
+	/**
+	 * @return the phoneNumbers
+	 */
+	public List<PhoneNumber> getPhoneNumbers() {
+		return new LinkedList<PhoneNumber>(phoneNumbers);
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+	
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	public boolean equals(Object obj) {
+		if (obj instanceof Person) {
+			Person p = (Person) obj;
+			return p.getName().equals(this.getName());
+		}
+		
+		return false;
+	}
+	
+	public String toString() {
+		return getName().toString();
+	}
+	
+}
---
+++ trunk/pelore/test/test/unit/PersonTest.java
@@ -0,0 +1,69 @@
+package test.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPersonString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPersonStringAddress() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testPhoneNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetPhoneNumbers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetAddress() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetAddress() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
