e44565af7b6ca79a24741f3a4e64cf35a02d5a38
thiagossantos <thiagossantos@75052923-3742-0410-8fdd-33ae7300e995>
2007-12-28 04:58:50 +0000

    Created User class and a stub test class

---
+++ trunk/pelore/test/test/unit/UserTest.java
@@ -0,0 +1,49 @@
+package test.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testTestPassword() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testChangePassword() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
